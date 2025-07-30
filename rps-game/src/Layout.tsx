2b9e8259cf898b9749464d987f16b3f7bb9f807d
Ali <foraliasadi@gmail.com>
2025-07-30 21:17:00 +0330

    fixed buttons

--- rps-game/src/Layout.tsx
+++ rps-game/src/Layout.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from "sonner";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   return (
     <div>
       <Toaster position="top-center" richColors /> <Navbar />
       <Outlet />
+      <Analytics />
     </div>
   );
 }
